2f09bf575e669cd56b8ac28b7c81111c7bcc5962
arthas0809 <arthas0809@highgateschool.org.uk>
2025-05-14 13:01:05 +0100

    create slime class

--- kivy first try/evolution sim.py
+++ kivy first try/evolution sim.py
@@ -1,16 +1,78 @@
+#https://stackoverflow.com/questions/21565994/method-to-return-the-equation-of-a-straight-line-given-two-points
+
+#imports
+import random
 import pygame
 
+#objects
 class Slime:
     def __init__(self,
                  speed,
                  max_hunger,
                  colour,
                  size,
-                 agression):
+                 agression,
+                 cx,
+                 cy):
         self.speed = speed
         self.max_hunger = max_hunger
         self.colour = colour
         self.size = size
         self.agression = agression
+        self.cx = cx
+        self.cy = cy
+    #function to create slimes at start
     def create(self):
-        pygame.draw.
+        pygame.draw.circle(screen,self.colour,(self.cx,self.cy),self.size)
+
+    def selectlocation(self):
+        locationX = random.randint(0,1360)
+        locationY = random.randint(0,980)
+
+        return locationX,locationY
+
+    def move(self):
+        moving = False
+        while not moving
+            posX,posY = self.selectlocation()
+
+
+
+
+
+#set screen size
+screen = pygame.display.set_mode((1360, 980))
+running = True
+
+#set start attributes
+start_size = 50
+
+#create slimes at start
+my_slime = Slime(10,
+                 10,
+                 "red",
+                 start_size,
+                 1,
+                 random.randint(start_size,screen.get_width()-start_size),
+                 random.randint(start_size,screen.get_height()-start_size))
+
+
+#while the program is playing
+while running:
+
+    #user inputs
+    for event in pygame.event.get():
+
+        #check if user quits the program
+        if event.type == pygame.QUIT:
+            running = False
+
+    #fill screen colour
+    screen.fill((0, 0, 0))
+
+    #Slime actions
+    my_slime.create()
+
+
+
+    pygame.display.update()
